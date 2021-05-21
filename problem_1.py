9ebe05d3e9430103e3fc2aaefc9aff278b979eb2
Samuel  Olufemi <samuel.olufemi@iisysgroup.com>
2021-05-21 20:44:56 +0100

    Assignment Submit update

--- problem_1.py
+++ problem_1.py
@@ -67,11 +67,11 @@ print(our_cache3.get(2))       # returns 2
 print(our_cache3.get(9))      # returns -1 because 9 is not present in the cache
 
 
-print(our_cache4.get(1))       # returns 1
+print(our_cache4.get(1))       # returns -1
 print(our_cache4.get(2))       # returns 2
 print(our_cache4.get(9))      # returns -1 because 9 is not present in the cache
 
 our_cache.set(5, 5) 
 our_cache.set(6, 6)
 
-print(our_cache.get(3))      
+print(our_cache.get(3))      #3
